fc27c2cf2188138bdbc8909081c6aec9c2050e4d
zcfrank1st <zhchaos@gmail.com>
2018-05-21 21:23:48 +0800

    change code

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
 setup(
-    name='piggyz',
+    name='pigg',
     version='0.1',
     description='a machine learning api frame',
     author='zcfrank1st',
     author_email='zhchaos@gmail.com',
     url='https://github.com/zcfrank1st/piggy',
-    py_modules=['piggyz'],
+    py_modules=['pigg'],
+    packages=['frame', 'serv'],
+
+    install_requires=(
+       'tornado==4.5.3',
+    ),
 )
