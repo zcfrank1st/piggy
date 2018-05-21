0157d15442c51b38656c95990de7488951145c2d
zcfrank1st <zhchaos@gmail.com>
2018-05-21 20:12:55 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='piggys',
+    version='0.1',
+    description='a machine learning api frame',
+    author='zcfrank1st',
+    author_email='zhchaos@gmail.com',
+    url='https://github.com/zcfrank1st/piggy',
+    py_modules=['piggys'],
+)
